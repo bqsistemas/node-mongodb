4f02183647667f17f022d8bcb2daccc6bcaba7f4
Jhon Barrantes <jbarrantes@board.pe>
2022-06-17 12:47:46 -0500

    Feat: Crud implemented on circulation repository

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
 const circulationRepo = require('./repos/circulationRepo')
 const data = require('./circulation.json')
@@ -8,15 +9,36 @@ const dbName = 'circulation'
 
 async function main() {
     const client = new MongoClient(url)
-    await client.connect()
+    try{
+        await client.connect()
 
-    const results = await circulationRepo.loadData(data)
-    console.log(results.insertedCount, results.ops)
+        const results = await circulationRepo.loadData(data)
+        assert.equal(data.length, results.insertedCount)
 
+        // get data
+        const getData = await circulationRepo.get({}, 50)
+        assert.equal(data.length, getData.length)
 
-    const admin = client.db(dbName).admin()
-    // console.log(await admin.serverStatus())
-    console.log(await admin.listDatabases())
+        // filter data
+        const filterData = await circulationRepo.get({Newspaper: getData[4].Newspaper}, 50)
+        assert.equal(filterData[0].Newspaper, getData[4].Newspaper)
+        assert.deepEqual(filterData[0], getData[4])
+
+        // limit data
+        const limitData = await circulationRepo.get({}, 3)
+        assert.equal(limitData.length, 3)
+        
+    } catch(error) {
+        console.log(error)
+    } finally {
+        const admin = client.db(dbName).admin()
+        await client.db(dbName).dropDatabase() // drop databases
+
+        // console.log(await admin.serverStatus())
+        console.log(await admin.listDatabases())
+
+        client.close()
+    }
 }
 
 main();
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -4,6 +4,114 @@ function circulationRepo() {
   const url = 'mongodb://bqsistemas:barrantes@127.0.0.1:27017';
   const dbName = 'circulation';
 
+  function get(query, limit) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        let items = db.collection('newspapers').find(query);
+        // collection.find({}).project({ a: 1 })                             // Create a projection of field a
+        // collection.find({}).skip(1).limit(10)                          // Skip 1 and limit 10
+        // collection.find({}).batchSize(5)                               // Set batchSize on cursor to 5
+        // collection.find({}).filter({ a: 1 })                              // Set query on the cursor
+        // collection.find({}).comment('add a comment')                   // Add a comment to the query, allowing to correlate queries
+        // collection.find({}).addCursorFlag('tailable', true)            // Set cursor as tailable
+        // collection.find({}).addCursorFlag('oplogReplay', true)         // Set cursor as oplogReplay
+        // collection.find({}).addCursorFlag('noCursorTimeout', true)     // Set cursor as noCursorTimeout
+        // collection.find({}).addCursorFlag('awaitData', true)           // Set cursor as awaitData
+        // collection.find({}).addCursorFlag('exhaust', true)             // Set cursor as exhaust
+        // collection.find({}).addCursorFlag('partial', true)             // Set cursor as partial
+        // collection.find({}).addQueryModifier('$orderby', { a: 1 })        // Set $orderby {a:1}
+        // collection.find({}).max(10)                                    // Set the cursor max
+        // collection.find({}).maxTimeMS(1000)                            // Set the cursor maxTimeMS
+        // collection.find({}).min(100)                                   // Set the cursor min
+        // collection.find({}).returnKey(10)                              // Set the cursor returnKey
+        // collection.find({}).setReadPreference(ReadPreference.PRIMARY)  // Set the cursor readPreference
+        // collection.find({}).showRecordId(true)                         // Set the cursor showRecordId
+        // collection.find({}).sort([['a', 1]])                           // Sets the sort order of the cursor query
+        // collection.find({}).hint('a_1')                                // Set the cursor hint
+        if (limit > 0) {
+          items = items.limit(limit);
+        }
+        resolve(await items.toArray());
+      } catch (error) {
+        reject(error);
+      } finally {
+        client.close();
+      }
+    });
+  }
+
+  function getById(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const item = await db.collection('newspapers').findOne({ _id: ObjectID(id) });
+        resolve(item);
+      } catch (error) {
+        reject(error);
+      } finally {
+        client.close();
+      }
+    });
+  }
+
+  function add(item) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const addedItem = await db.collection('newspapers').insertOne(item);
+
+        resolve(addedItem.ops[0]);
+      } catch (error) {
+        reject(error);
+      } finally {
+        client.close();
+      }
+    });
+  }
+
+  function update(id, newItem) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const updatedItem = await db.collection('newspapers')
+          .findOneAndReplace({_id: ObjectID(id)}, newItem, {returnOriginal:false});
+        
+        resolve(updatedItem.value);
+      } catch (error) {
+        reject(error);
+      } finally {
+        client.close();
+      }
+    });
+  }
+
+  function remove(id){
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const removed = await db.collection('newspapers').deleteOne({_id: ObjectID(id)});
+
+        resolve(removed.deletedCount === 1);
+      } catch (error) {
+        reject(error);
+      } finally {
+        client.close();
+      }
+    });
+  }
+
   function loadData(data) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
@@ -16,11 +124,13 @@ function circulationRepo() {
 
       } catch (error) {
         reject(error)
+      } finally {
+        client.close();
       }
     })
   }
 
-  return { loadData }
+  return { loadData, get, getById, add, update, remove }
 
 }
 
